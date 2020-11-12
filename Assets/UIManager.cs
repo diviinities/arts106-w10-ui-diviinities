56960138cd3ec63a65c38e7f2295d8de750d5079
diviinities <71028707+diviinities@users.noreply.github.com>
2020-11-11 21:14:40 -0500

    settings and play buttons animated

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -5,9 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator startButton;
+    public Animator settingsButton;
     // Start is called before the first frame update
     public void StartGame()
     {
         SceneManager.LoadScene("RocketMouse");
+    }
+    public void OpenSettings()
+    {
+        startButton.SetBool("ishidden", true);
+        settingsButton.SetBool("ishidden", true);
     }
 }
